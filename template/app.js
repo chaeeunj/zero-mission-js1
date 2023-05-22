f98a237c3527eb4c3e3381a88a8e9ee991e3b2e4
chaeeunj <official2930@naver.com>
2023-05-22 18:35:33 +0900

    버튼 클릭 시 .active 클래스 추가되는 것만 구현

--- template/app.js
+++ template/app.js
@@ -1,1 +1,18 @@
 // do something!
+
+// const arrowBtn = document.querySelector('.toggle');
+// arrowBtn.addEventListener('click', openNav);
+
+// function openNav() {
+//   document.querySelector('.navigation').style.width = '200px';
+//   document.querySelector('.navigation').style.backgroundColor = 'black';
+// }
+
+const nav = document.querySelector('nav');
+const arrowBtn = document.querySelector('.toggle');
+
+arrowBtn.addEventListener('click', addClass);
+
+function addClass() {
+  nav.classList.toggle('active');
+}
