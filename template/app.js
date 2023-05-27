5c04de667654623e636f563aba271e9192085579
chaeeunj <official2930@naver.com>
2023-05-28 04:38:56 +0900

    toggle side nav 구현

--- template/app.js
+++ template/app.js
@@ -1,18 +1,17 @@
 // do something!
 
-// const arrowBtn = document.querySelector('.toggle');
-// arrowBtn.addEventListener('click', openNav);
-
-// function openNav() {
-//   document.querySelector('.navigation').style.width = '200px';
-//   document.querySelector('.navigation').style.backgroundColor = 'black';
-// }
-
 const nav = document.querySelector('nav');
 const arrowBtn = document.querySelector('.toggle');
 
-arrowBtn.addEventListener('click', addClass);
+arrowBtn.addEventListener('click', isActive);
 
-function addClass() {
+function isActive() {
   nav.classList.toggle('active');
+  nav.classList.contains('active')
+    ? localStorage.setItem('navMenu', 'open')
+    : localStorage.setItem('navMenu', 'close');
 }
+
+localStorage.getItem('navMenu') === 'open'
+  ? (nav.classList.add('active'), (document.body.style.visibility = 'visible'))
+  : (document.body.style.visibility = 'visible');
